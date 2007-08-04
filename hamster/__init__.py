62fa96bf60af885537e3627ce27c93c2a031c425
Toms Baugis <tbaugis@src.gnome.org>
2007-08-04 16:33:54 +0000

    install hamsterdb on launch if it was lost somehow or if it is first time running
    svn path=/trunk/; revision=7

--- hamster/__init__.py
+++ hamster/__init__.py
@@ -1,5 +1,6 @@
 import os, sys
 from os.path import join, exists, isdir, isfile, dirname, abspath, expanduser
+from shutil import copy as copyfile
 
 import gtk
 
@@ -30,11 +31,18 @@ else:
 print "Data Dir: %s" % SHARED_DATA_DIR
 
 USER_HAMSTER_DIR = expanduser("~/.gnome2/hamster-applet")
+DB_FILE = 'hamster.db'
+HAMSTER_DB = join(USER_HAMSTER_DIR, DB_FILE)
 if not exists(USER_HAMSTER_DIR):
     try:
         os.makedirs(USER_HAMSTER_DIR, 0744)
     except Exception , msg:
         print 'Error:could not create user dir (%s): %s' % (USER_HAMSTER_DIR, msg)
+
+#check if db is here
+if not exists(HAMSTER_DB):
+    print "Database not found in %s - installing default from %s!" % (HAMSTER_DB, SHARED_DATA_DIR)
+    copyfile(join(SHARED_DATA_DIR, DB_FILE), HAMSTER_DB)
 
 # Path to images, icons
 ART_DATA_DIR = join(SHARED_DATA_DIR, "art")
