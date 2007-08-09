8926663010d1e177dfca87b451f01c5bf398c7e9
Toms Baugis <tbaugis@src.gnome.org>
2007-08-09 12:58:57 +0000

    fixed bug with adding duplicates. also be more efficient and do not touch database on each hover
    svn path=/trunk/; revision=18

--- hamster/HamsterApplet.py
+++ hamster/HamsterApplet.py
@@ -10,9 +10,11 @@ class HamsterApplet(object):
         self.applet = applet
         self.label = gtk.Label("Hamster")
 
+        self.last_activity = hamster.db.get_last_activity()
+
         self.menu = gtk.Menu()
         # build the menu
-        self.activities = update_menu(self.menu)
+        self.activities = self.update_menu(self.menu)
 
 
         self.evBox = gtk.EventBox()
@@ -34,40 +36,76 @@ class HamsterApplet(object):
 
         self.applet.show_all()
 
+    def update_menu(self, menu):
+        #remove all items
+        children = menu.get_children()
+        for child in children:
+            menu.remove(child)
+
+        #populate fresh list from DB
+        activities = hamster.db.get_activity_list()
+        prev_item = None
+
+        items = []
+        for activity in activities:
+            item = gtk.RadioMenuItem(prev_item, activity['name'])
+
+            #set selected
+            if activity['name'] == self.last_activity['name']:
+                item.set_active(True);
+
+            item.connect("activate", self.changeActivity, activity['id'])
+            menu.add(item)
+            prev_item = item
+            items.append({'id':activity['id'], 'name':activity['name']})
+
+        separator = gtk.SeparatorMenuItem()
+        menu.add(separator)
+
+        menu_edit_activities = gtk.MenuItem('Edit activities');
+        menu_edit_activities.connect("activate", edit_activities)
+        menu.add(menu_edit_activities)
+
+        menu_show_overview = gtk.MenuItem('Overview');
+        menu_show_overview.connect("activate", show_overview)
+        menu.add(menu_show_overview)
+
+        menu.show_all()
+
+        return items
+
     def clicked(self, event_box, event):
         if event.button == 1:
             self.menu.popup(None, None, None, event.button, gtk.get_current_event_time())
 
     def on_tooltip(self, event_box, event):
-        print "omg, i'm here!"
-        last_activity, duration = hamster.db.get_last_activity()
+        today = time.strftime('%Y%m%d')
+        now = time.strftime('%H%M')
 
-        if duration == 0:
+        if (self.last_activity['fact_date'] != int(today)):
             tooltip = "Nothing done today!"
-        elif duration < 1:
-            tooltip = "Just started '%s'!" % (last_activity)
         else:
-            if duration < 60:
-                duration = "%d minutes" % (duration)
-            else:
-                duration = "%.1fh hours" % (duration / 60.0)
+            # we are adding 0.1 because we don't have seconds but
+            # would like to differ between nothing and just started something
+            duration = hamster.db.mins(now) - hamster.db.mins(self.last_activity['fact_time']) + 0.1
 
-            tooltip = "You have been doing '%s' for %s" % (last_activity, duration)
+            if duration < 1:
+                tooltip = "Just started '%s'!" % (self.last_activity['name'])
+            else:
+                if duration < 60:
+                    duration = "%d minutes" % (duration)
+                else:
+                    duration = "%.1fh hours" % (duration / 60.0)
+
+                tooltip = "You have been doing '%s' for %s" % (self.last_activity['name'], duration)
 
         self.tooltip.set_tip(event_box, tooltip)
-
-        # now let's run through menu items and see, maybe we can mark one of them
-        i = 0
-        for item in self.menu:
-            if len(self.activities) > i and self.activities[i]['name'] == last_activity:
-                item.set_active(True)
-
-            i = i +1
-
 
     def on_about (self, component, verb):
         show_about(self.applet)
 
+    def changeActivity(self, menu, activity_id):
+        self.last_activity = hamster.db.add_fact(activity_id)
 
 def edit_activities(menu_item):
     from hamster.activities import ActivitiesEditor
@@ -95,39 +133,6 @@ def activities_reordered_cb(model, path, row1, row2, menu):
 def activity_deleted_cb(model, path, menu):
     update_menu(menu)
 
-def changeActivity(menu, activity_id):
-    hamster.db.add_fact(activity_id)
 
-def update_menu(menu):
-    #remove all items
-    children = menu.get_children()
-    for child in children:
-        menu.remove(child)
-
-    #populate fresh list from DB
-    activities = hamster.db.get_activity_list()
-    prev_item = None
-    items = []
-    for activity in activities:
-        item = gtk.RadioMenuItem(prev_item, activity['name'])
-        item.connect("activate", changeActivity, activity['id'])
-        menu.add(item)
-        prev_item = item
-        items.append({'id':activity['id'], 'name':activity['name']})
-
-    separator = gtk.SeparatorMenuItem()
-    menu.add(separator)
-
-    menu_edit_activities = gtk.MenuItem('Edit activities');
-    menu_edit_activities.connect("activate", edit_activities)
-    menu.add(menu_edit_activities)
-
-    menu_show_overview = gtk.MenuItem('Overview');
-    menu_show_overview.connect("activate", show_overview)
-    menu.add(menu_show_overview)
-
-    menu.show_all()
-
-    return items
 
 
