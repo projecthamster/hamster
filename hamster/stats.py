c4ffea11d9b369fb1e544c32740b493d6601928c
Toms Baugis <tbaugis@src.gnome.org>
2008-03-21 16:29:54 +0000

    dropped matplotlib and now doing all in cairo, still - work in progress!
    svn path=/trunk/; revision=110

--- hamster/stats.py
+++ hamster/stats.py
@@ -8,15 +8,7 @@ import gtk.glade
 
 from hamster import dispatcher, storage, SHARED_DATA_DIR
 
-import matplotlib
 import datetime  as dt
-matplotlib.use('gtkAgg')
-from matplotlib.backends.backend_gtk import FigureCanvasGTK as FigureCanvas
-from matplotlib import rc
-from matplotlib.figure import Figure
-import matplotlib.dates
-from matplotlib.dates import drange, DateFormatter, DayLocator, MonthLocator, YearLocator, date2num
-
 import math
 
 #some hand-picked tango colors
@@ -39,6 +31,7 @@ dark = [(196, 160, 0), (78, 154, 6), (206, 92, 0),
         (32, 74, 135), (92, 53, 102), (143, 89, 2),
         (164, 0, 0), (186, 189, 182), (46, 52, 54)]
 
+color_count = len(light)
 
 def set_color(context, color):
     r,g,b = color[0] / 255.0, color[1] / 255.0, color[2] / 255.0
@@ -46,7 +39,7 @@ def set_color(context, color):
     
     
 class Chart(gtk.DrawingArea):
-    def __init__(self, vertical = True):
+    def __init__(self, **args):
         gtk.DrawingArea.__init__(self)
         self.connect("expose_event", self.expose)
         self.data = None #start off with an empty hand
@@ -56,15 +49,20 @@ class Chart(gtk.DrawingArea):
         self.step = 35 # distance between one and the next bar
         self.bar_width = 30 #bar width
         
-        self.offset_x = 50 # graph offset in pixels inside canvas
-        self.offset_y = 50 # graph offset in pixels inside canvas
+        #now see what we have in args!
+        self.cycle_colors = "cycle_colors" in args and args["cycle_colors"]
+        self.orient_vertical = "orient" not in args or args["orient"] == "vertical"
         
     def expose(self, widget, event): # expose is when drawing's going on
         context = widget.window.cairo_create()
         context.rectangle(event.area.x, event.area.y, event.area.width, event.area.height)
         context.clip()
         
-        self.draw(context)
+        if self.orient_vertical:
+            self.bar_chart(context) 
+        else:
+            self.horizontal_bar_chart(context)
+
         return False
 
     def plot(self, data):
@@ -77,63 +75,171 @@ class Chart(gtk.DrawingArea):
             self.window.process_updates(True)
         
     
-    def draw(self, context):
+    def bar_chart(self, context):
         rect = self.get_allocation()  #x, y, width, height        
+        data, records = self.data, len(self.data)
 
-        graph_x = rect.x + self.offset_x
-        graph_y = rect.y + self.offset_y
-
+        graph_x = rect.x + 50  #give some space to scale labels
 
         context.set_line_width(1)
         
-        data = self.data
         
         #find max, so we know
-        max = 0.1
+        max_value = 0.1
         for i in data:
-            if i[1] > max: max = i[1] * 1.0
+            max_value = max(max_value, i[1] * 1.0)
         
 
         # TODO put this somewhere else - drawing background and some grid
-        context.rectangle(graph_x - 1, rect.y, self.step * len(data), 101)
+        context.rectangle(graph_x - 1, rect.y, self.step * records, self.max_size + 1)
         context.set_source_rgb(1, 1, 1)
         context.fill_preserve()
+        context.stroke()
 
         context.set_line_width(1)
         context.set_dash ([1, 3]);
 
         set_color(context, dark[8])
         
+        # scale lines
         for y in range(rect.y, rect.y + self.max_size + 5, self.max_size / 3):
             context.move_to(graph_x - 10, y)
-            context.line_to(graph_x + self.step * len(data), y)
+            context.line_to(graph_x + self.step * records, y)
+
+        # and borders on both sides, so the graph doesn't fall out
+        context.move_to(graph_x - 1, rect.y)
+        context.line_to(graph_x - 1, rect.y + self.max_size + 1)
+        context.move_to(graph_x + self.step * records, rect.y)
+        context.line_to(graph_x + self.step * records, rect.y + self.max_size + 1)
+        
 
         context.stroke()
         
         
         context.set_dash ([]);
         # bars themselves
-        for i in range(len(data)):
+        for i in range(records):
             context.rectangle(graph_x + (self.step * i),
-                              rect.y + self.max_size - (self.max_size * (data[i][1] / max)),
+                              rect.y + self.max_size - (self.max_size * (data[i][1] / max_value)),
                               self.bar_width,
-                              (self.max_size * (data[i][1] / max)))
+                              (self.max_size * (data[i][1] / max_value)))
 
-        set_color(context, light[1])
-        context.fill_preserve()
-
-        set_color(context, dark[1]);
-        context.stroke()
+            color = 1
+            if self.cycle_colors:
+                if color_count < records:
+                    color = (i + 1) % color_count
+                else:
+                    color = i
+                
+            set_color(context, light[color])
+            context.fill_preserve()    
+            set_color(context, dark[color]);
+            context.stroke()
         
         # labels
         set_color(context, dark[8]);
-        for i in range(len(data)):
+        for i in range(records):
             context.move_to(graph_x + 5 + (self.step * i), rect.y + self.max_size + 15)
             context.show_text(data[i][0])
 
         # values for max minn and average
         context.move_to(rect.x + 10, rect.y + 10)
-        context.show_text(str(max))
+        context.show_text(str(max_value))
+
+
+
+
+    def horizontal_bar_chart(self, context):
+        rect = self.get_allocation()  #x, y, width, height
+        data, records = self.data, len(self.data)
+        
+        
+        
+        # ok, start with labels - get the longest now
+        # TODO - figure how to wrap text
+        max_extent, max_value = 0, 0.1
+        for i in range(records):
+            extent = context.text_extents(data[i][0]) #x, y, width, height
+            max_extent = max(max_extent, extent[2])
+            max_value = max(max_value, data[i][1] * 1.0)
+            
+        
+
+        #now let's put the labels and align them right
+        set_color(context, dark[8]);
+        for i in range(records):
+            extent = context.text_extents(data[i][0]) #x, y, width, height
+            
+            context.move_to(rect.x + max_extent - extent[2], rect.y + (self.step * i) + 15)
+            context.show_text(data[i][0])
+        
+        context.stroke()        
+        
+        
+        #push graph to the right, so it doesn't overlap, and add little padding aswell
+        graph_x = rect.x + max_extent + 10
+        graph_width = rect.width + rect.x - graph_x - 40
+
+        graph_y = rect.y
+        graph_height = graph_y - rect.x + rect.height
+        
+        self.max_size = graph_width - 15
+
+
+        context.set_line_width(1)
+        
+        
+        # TODO put this somewhere else - drawing background and some grid
+        context.rectangle(graph_x - 1, rect.y, graph_width, graph_height)
+        context.set_source_rgb(1, 1, 1)
+        context.fill_preserve()
+        context.stroke()
+
+
+        context.set_line_width(1)
+        context.set_dash ([1, 3]);
+
+        set_color(context, dark[8])
+
+        # scale lines        
+        for x in range(graph_x, graph_x + graph_width, int(graph_width / 4)):
+            context.move_to(x-1, graph_y)
+            context.line_to(x-1, graph_y + graph_height)
+
+        # and borders on both sides, so the graph doesn't fall out
+        context.move_to(graph_x, graph_y)
+        context.line_to(graph_x + graph_width, graph_y)
+        context.move_to(graph_x, graph_y + graph_height)
+        context.line_to(graph_x + graph_width, graph_y + graph_height)
+
+
+        context.stroke()
+        
+        
+        context.set_dash ([]);
+        # bars themselves
+        for i in range(records):
+            context.rectangle(graph_x,
+                              rect.y + (self.step * i) + 5,
+                              (self.max_size * (data[i][1] / max_value)),
+                              self.bar_width)
+
+            color = 1
+            if self.cycle_colors:
+                if color_count < records:
+                    color = (i + 1) % color_count
+                else:
+                    color = i
+                
+            set_color(context, light[color])
+            context.fill_preserve()    
+            set_color(context, dark[color]);
+            context.stroke()
+        
+
+        # values for max minn and average
+        context.move_to(graph_x + graph_width + 10, graph_y + 10)
+        context.show_text(str(max_value))
 
 class StatsViewer:
     def __init__(self):
@@ -175,6 +281,18 @@ class StatsViewer:
         eventBox.add(self.day_chart);
         place.add(eventBox)
         
+        self.category_chart = Chart(orient = "horizontal")
+        eventBox = gtk.EventBox()
+        place = self.get_widget("cairo_by_category")
+        eventBox.add(self.category_chart);
+        place.add(eventBox)
+        
+        self.activity_chart = Chart(orient = "horizontal")
+        eventBox = gtk.EventBox()
+        place = self.get_widget("cairo_by_activity")
+        eventBox.add(self.activity_chart);
+        place.add(eventBox)
+        
         
         self.do_graph()
 
@@ -199,8 +317,8 @@ class StatsViewer:
         totals = {}
         
         totals['by_day'] = []
-        totals['by_activity'] = {}
-        totals['by_category'] = {}
+        by_activity = {}
+        by_category = {}
         
         week = {"days": [], "totals": []}
             
@@ -219,16 +337,29 @@ class StatsViewer:
 
                     self.fact_store.append(day_row, [fact["name"], self.format_duration(duration)])
                     
-                    if fact["name"] not in totals['by_activity']: totals['by_activity'][fact["name"]] = 0
-                    if fact["category"] not in totals['by_category']: totals['by_category'][fact["category"]] = 0
+                    if fact["name"] not in by_activity: by_activity[fact["name"]] = 0
+                    if fact["category"] not in by_category: by_category[fact["category"]] = 0
 
-                    totals['by_activity'][fact["name"]] += duration
-                    totals['by_category'][fact["category"]] += duration
+                    by_activity[fact["name"]] += duration
+                    by_category[fact["category"]] += duration
                     day_total += duration
 
             totals['by_day'].append((current_date.strftime('%a'), day_total / 60))
             week["days"].append({"date": current_date, "strdate": strdate, "facts": facts})
+        
+        totals["by_category"] = []
+        for category in by_category:
+            totals["by_category"].append((category, by_category[category] / 60))
             
+        totals["by_activity"] = []
+        for activity in by_activity:
+            totals["by_activity"].append((activity, by_activity[activity] / 60))
+            
+        duration_sort = lambda a, b: b[1] - a[1]
+        
+        totals["by_category"].sort(duration_sort)
+        totals["by_activity"].sort(duration_sort)
+        
 
         self.fact_tree.expand_all()
 
@@ -264,75 +395,9 @@ class StatsViewer:
         week = self.get_week()
 
         self.day_chart.plot(week["totals"]["by_day"])
+        self.category_chart.plot(week["totals"]["by_category"])
+        self.activity_chart.plot(week["totals"]["by_activity"])
 
-        fig = Figure()
-
-        category_totals = week["totals"]["by_category"]
-        
-        # The numbers here are margins from the edge of the graph
-        # You may need to adjust this depending on how you want your
-        # graph to look, and how large your labels are
-        ax = fig.add_axes([0.5, 0.1, 0.45, 0.8])
-        
-        i = 0
-        labels = {}
-        for category in category_totals:
-            ax.barh(i, category_totals[category] / 60, color=color_list[i])
-            labels[i] = category
-            i = i + 1
-
-
-        ax.set_yticks(range(len(category_totals)+1))
-        ax.set_yticklabels(labels, va="bottom")
-        
-
-        self.canvas = FigureCanvas(fig) # a gtk.DrawingArea   
-        self.canvas.show()   
-        self.graphview = self.wTree.get_widget("by_category")
-        
-        kids = self.graphview.get_children()
-        if kids:
-            self.graphview.remove(kids[0])
-        
-        self.graphview.pack_start(self.canvas, True, True)
-
-
-
-
-
-
-
-
-        fig = Figure()
-
-        totals = week["totals"]["by_activity"]
-        
-        # The numbers here are margins from the edge of the graph
-        # You may need to adjust this depending on how you want your
-        # graph to look, and how large your labels are
-        ax = fig.add_axes([0.5, 0.1, 0.45, 0.8])
-        
-        i = 0
-        labels = {}
-        for entry in totals:
-            ax.barh(i, totals[entry] / 60, color=color_list[i])
-            labels[i] = entry
-            i = i + 1
-
-
-        ax.set_yticks(range(len(totals)+1))
-        ax.set_yticklabels(labels, va="bottom")
-        
-
-        self.canvas = FigureCanvas(fig) # a gtk.DrawingArea   
-        self.canvas.show()   
-        self.graphview = self.wTree.get_widget("by_activity")
-        
-        kids = self.graphview.get_children()
-        if kids:
-            self.graphview.remove(kids[0])
-        
-        self.graphview.pack_start(self.canvas, True, True)
 
 
 
