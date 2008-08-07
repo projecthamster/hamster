bbcf86d27fff1e24a4374c4438a46c55bc046442
Patryk Zawadzki <pzawadzki@src.gnome.org>
2008-08-07 18:51:09 +0000

    - add missing licenses as per Ubuntu's request
    svn path=/trunk/; revision=357

--- hamster/keybinder/_keybindermodule.c
+++ hamster/keybinder/_keybindermodule.c
@@ -1,4 +1,22 @@
-/* -*- Mode: C; c-basic-offset: 4 -*- */
+# - coding: utf-8 -
+
+# Copyright (C) 2005 Nigel Tao <nigeltao at cvs.gnome.org>
+
+# This file is part of Project Hamster.
+
+# Project Hamster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Project Hamster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Project Hamster.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
 #endif
--- hamster/keybinder/tomboykeybinder.h
+++ hamster/keybinder/tomboykeybinder.h
@@ -1,4 +1,20 @@
-
+/* tomboykeybinder.h
+ * Copyright (C) 2008 Novell
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA 
+ */
 #ifndef __TOMBOY_KEY_BINDER_H__
 #define __TOMBOY_KEY_BINDER_H__
 
