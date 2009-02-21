d4d66d689292acdca1a987ef972a107a75393af8
Toms Baugis <tbaugis@src.gnome.org>
2009-02-21 12:45:18 +0000

    right place
    svn path=/trunk/; revision=763

---
+++ hamster/tests/test_charting.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import unittest
+from ..hamster import charting
+
+class TestIteratorFunctions(unittest.TestCase):
+    def testOneStep(self):
+        integrator = charting.Integrator(0)
+        integrator.target(10)
+        
+        integrator.update()
+        assert integrator.value == 9
+
+if __name__ == '__main__':
+    unittest.main()
