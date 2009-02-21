5d004c34cd84c558a8b10cb4538df7dbb8952078
Toms Baugis <tbaugis@src.gnome.org>
2009-02-21 13:09:08 +0000

    fixed path for imports
    svn path=/trunk/; revision=768

--- tests/charting_test.py
+++ tests/charting_test.py
@@ -1,4 +1,6 @@
-import sys; sys.path.insert(0, "../..")
+import sys, os.path
+# a convoluted line to add hamster module to absolute path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import unittest
 from hamster import charting
