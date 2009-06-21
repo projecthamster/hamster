03060d10595c9027eb73ec97cd2663c220943f15
Toms Bauģis <toms.baugis@gmail.com>
2009-06-21 18:15:38 +0100

    have minutes in sup and small

--- hamster/charting.py
+++ hamster/charting.py
@@ -699,7 +699,7 @@ class HorizontalDayChart(Chart):
             
             minutes = i % (24 * 60)
 
-            self.layout.set_text(dt.time(minutes / 60, minutes % 60).strftime("%H:%M"))
+            self.layout.set_markup(dt.time(minutes / 60, minutes % 60).strftime("%H<small><sup>%M</sup></small>"))
             label_w, label_h = self.layout.get_pixel_size()
 
             context.move_to(self.graph_x + x - label_w / 2,
