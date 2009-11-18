96e39a19f10b2cc6172b53ec11d11e97f762c0ae
Toms Bauģis <toms.baugis@gmail.com>
2009-11-18 18:30:47 +0000

    rubbish

--- hamster/standalone.py
+++ hamster/standalone.py
@@ -39,8 +39,15 @@ class MainWindow(object):
         self.get_widget("tabs").set_current_page(1)
 
         self.set_last_activity()
+        self.load_today()
+        
+        gtk.link_button_set_uri_hook(self.magic)
+        
         self._gui.connect_signals(self)
 
+    def magic(self, button, uri):
+        print uri, button
+        
     def style_widgets(self):
         #TODO - replace with the tree background color (can't get it atm!)
         self.get_widget("todays_activities_ebox").modify_bg(gtk.STATE_NORMAL,
@@ -60,17 +67,70 @@ class MainWindow(object):
             self.get_widget("last_activity_name").set_text(activity['name'])
             if activity['category'] != _("Unsorted"):
                 self.get_widget("last_activity_category") \
-                    .set_markup("<small>%s</small>" %
-                                       stuff.escape_pango(activity['category']))
+                    .set_text(" - %s" % activity['category'])
                 self.get_widget("last_activity_category").show()
             else:
-                self.get_widget("last_activity_category").show()
+                self.get_widget("last_activity_category").hide()
 
             if activity['description']:
                 self.get_widget("last_activity_description").set_text(activity['description'])
                 self.get_widget("last_activity_description").show()
             else:
                 self.get_widget("last_activity_description").hide()
+
+    def load_today(self):
+        todays_facts = runtime.storage.get_facts(dt.date.today())
+        grid = self.get_widget("activities_today")
+        
+        #clear
+        for child in grid.get_children(): grid.remove(child)
+        
+
+        def add_cell(cell, x, y, expandX = gtk.FILL,
+                                 expandY = gtk.FILL):
+
+            if isinstance(cell, str) or isinstance(cell, unicode):
+                cell = gtk.Label(cell)
+                cell.set_alignment(0.0, 0.0)
+            
+            grid.attach(cell, x, x + 1, y, y + 1, expandX, expandY)
+
+            
+        for rownum, fact in enumerate(todays_facts):
+            if fact["end_time"]:
+                fact_time = "%s - %s " % (fact["start_time"].strftime("%H:%M"),
+                                       fact["end_time"].strftime("%H:%M"))
+            else:
+                fact_time = fact["start_time"].strftime("%H:%M ")
+
+            duration = 24 * 60 * fact["delta"].days + fact["delta"].seconds / 60
+
+            add_cell(fact_time, 0, rownum)
+
+            name_tags = gtk.VBox()
+            name_label = gtk.Label(fact['name'])
+            name_label.set_alignment(0, 0)
+            name_tags.pack_start(name_label)
+
+            description_label = gtk.Label(fact['category'])
+            description_label.set_alignment(0, 0)
+            name_tags.pack_start(description_label)
+
+            add_cell(name_tags, 1, rownum, (gtk.EXPAND | gtk.FILL), ())
+
+            add_cell(stuff.format_duration(duration), 2, rownum, gtk.FILL, ())
+
+            resume_button = gtk.LinkButton("resume:%d" % fact['id'])
+            resume_button.set_label(_("Resume"))
+            add_cell(resume_button, 3, rownum, (), ())
+
+            resume_button = gtk.LinkButton("edit:%d" % fact['id'])
+            resume_button.set_label(_("Edit"))
+            add_cell(resume_button, 4, rownum, (), ())
+
+
+
+        
 
     def on_switch_activity_clicked(self, widget):
         self.get_widget("new_entry_box").show()
