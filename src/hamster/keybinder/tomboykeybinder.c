becbf7b530fbad275b1c4a03cfe431e1fa8f7e8a
Toms Bauģis <toms.baugis@gmail.com>
2010-05-07 22:26:06 +0100

    fixed bug 617994 - something about keybinders and metas

--- src/hamster/keybinder/tomboykeybinder.c
+++ src/hamster/keybinder/tomboykeybinder.c
@@ -129,6 +129,8 @@ do_grab_key (Binding *binding)
 	egg_keymap_resolve_virtual_modifiers (keymap,
 					      virtual_mods,
 					      &binding->modifiers);
+	if (binding->keycode == 0)
+		return FALSE;
 
 	TRACE (g_print ("Got modmask %d\n", binding->modifiers));
 
