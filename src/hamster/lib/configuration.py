803b79624aa855e720f8a41e9749948192293176
Nikita Kazeev <kazeevn@yandex-team.ru>
2014-07-18 16:41:04 +0400

    Added a note about window focus to Readme.

--- src/hamster/lib/configuration.py
+++ src/hamster/lib/configuration.py
@@ -6,7 +6,7 @@
 
 # Project Hamster is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# the Free oftware Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
 # Project Hamster is distributed in the hope that it will be useful,
