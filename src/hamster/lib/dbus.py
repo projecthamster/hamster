6f3a4dab7218d16a1f5d4a7e348760ad734d4b41
GeraldJansen <gjansen@ownmail.net>
2019-11-19 11:40:15 +0100

    Convert to int in fact_from_dbus

--- src/hamster/lib/dbus.py
+++ src/hamster/lib/dbus.py
@@ -28,13 +28,13 @@ fact_signature = '(iiissisasii)'
 def from_dbus_fact(dbus_fact):
     """unpack the struct into a proper dict"""
     return Fact(activity=dbus_fact[4],
-                start_time=dt.datetime.utcfromtimestamp(dbus_fact[1]),
-                end_time=dt.datetime.utcfromtimestamp(dbus_fact[2]) if dbus_fact[2] else None,
+                start_time=dt.datetime.utcfromtimestamp(int(dbus_fact[1])),
+                end_time=dt.datetime.utcfromtimestamp(int(dbus_fact[2])) if dbus_fact[2] else None,
                 description=dbus_fact[3],
-                activity_id=dbus_fact[5],
+                activity_id=int(dbus_fact[5]),
                 category=dbus_fact[6],
                 tags=dbus_fact[7],
-                id=dbus_fact[0]
+                id=int(dbus_fact[0])
                 )
 
 
